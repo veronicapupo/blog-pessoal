02ce685b5c39f0ea127b536ad2671667c8587b21
veronica pupo <veronica.pupo@hotmail.com>
2022-08-30 14:55:51 -0300

    implementando metodo getByid

---
+++ src/main/java/com/generation/blogpessoal/controller/PostagemController.java
@@ -0,0 +1,34 @@
+package com.generation.blogpessoal.controller;
+
+import com.generation.blogpessoal.model.Postagem;
+import com.generation.blogpessoal.repository.PostagemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/postagens")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+
+public class PostagemController {
+
+    @Autowired
+    private PostagemRepository postagemRepository;
+
+
+    @GetMapping
+    public ResponseEntity<List<Postagem>> getAll(){
+        return ResponseEntity.ok(postagemRepository.findAll());
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<Postagem> getById(@PathVariable Long id){
+        return postagemRepository.findById(id)
+                .map(resposta -> ResponseEntity.ok(resposta))
+                .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+    }
+}
+
+
