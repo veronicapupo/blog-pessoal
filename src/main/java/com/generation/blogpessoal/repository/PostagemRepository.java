4b98a194b0a071afdeec9039964111090cf54081
veronica pupo <veronica.pupo@hotmail.com>
2022-08-31 12:04:39 -0300

    inserindo post, put, delete

--- src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
+++ src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
@@ -3,11 +3,14 @@ package com.generation.blogpessoal.repository;
 
 import com.generation.blogpessoal.model.Postagem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface PostagemRepository extends JpaRepository<Postagem, Long> {
 
 
-    //public List<Postagem> findAllByTituloContainingIgnoreCase (String titulo);
+    List<Postagem> findAllByTituloContainingIgnoreCase (@Param("titulo") String titulo);
 }
